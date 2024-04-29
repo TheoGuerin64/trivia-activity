83ee081bd8c3abe7d2d14de890b82c56a2771609
TheoGuerin64 <57496441+TheoGuerin64@users.noreply.github.com>
2024-04-29 16:45:30 +0200

    Add a loading spinner to the app

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+
+import LoadingSpinner from './components/LoadingSpinner.vue'
 </script>
 
 <template>
-  <RouterView />
+  <router-view v-slot="{ Component }">
+    <template v-if="Component">
+      <KeepAlive>
+        <suspense timeout="0">
+          <component :is="Component" />
+          <template #fallback>
+            <LoadingSpinner />
+          </template>
+        </suspense>
+      </KeepAlive>
+    </template>
+  </router-view>
 </template>
---
+++ client/src/components/LoadingSpinner.vue
@@ -0,0 +1,13 @@
+<template>
+  <span class="loading" aria-busy="true" />
+</template>
+
+<style lang="scss">
+.loading {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 5rem;
+}
+</style>
