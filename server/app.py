16aa1f3d82850d7350b7ac2c1bf39310140ade1c
TheoGuerin64 <57496441+TheoGuerin64@users.noreply.github.com>
2024-05-01 04:42:40 +0200

    Replace aiohttp with uvicorn

--- server/app.py
+++ server/app.py
@@ -1,5 +1,4 @@
 import socketio
-from aiohttp import web
 from pydantic import ValidationError
 from socketio.exceptions import ConnectionRefusedError
 
@@ -8,11 +7,10 @@ from models import Auth
 from settings import DISCORD_CLIENT_ID
 
 sio = socketio.AsyncServer(
-    async_mode="aiohttp",
+    async_mode="asgi",
     cors_allowed_origins=f"https://{DISCORD_CLIENT_ID}.discordsays.com",
 )
-app = web.Application()
-sio.attach(app)
+app = socketio.ASGIApp(sio)
 
 
 @sio.event
@@ -26,7 +24,3 @@ async def connect(sid: str, environ: dict[str, str], raw_auth: dict[str, str]) -
         await discord.getToken(auth.code)
     except APIError as e:
         raise ConnectionRefusedError(str(e))
-
-
-if __name__ == "__main__":
-    web.run_app(app, port=3000)
