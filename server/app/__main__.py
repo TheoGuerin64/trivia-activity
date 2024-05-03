7fd96d1630462ad55102a20a0de9111334828800
TheoGuerin64 <57496441+TheoGuerin64@users.noreply.github.com>
2024-05-04 00:27:49 +0200

    Replace DEV environment variable with __debug__

--- server/app/__main__.py
+++ server/app/__main__.py
@@ -2,7 +2,6 @@ import uvicorn
 
 from . import schemas  # noqa: F401
 from .db import Base, engine
-from .settings import DEV
 
 Base.metadata.create_all(bind=engine)
-uvicorn.run("app:app", ws_max_size=2**12, host="0.0.0.0", port=3000, reload=DEV)
+uvicorn.run("app:app", ws_max_size=2**12, host="0.0.0.0", port=3000, reload=__debug__)
