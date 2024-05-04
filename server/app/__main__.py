b6f8175cdfa66a8438e0068641a3192ec8ea4ba9
TheoGuerin64 <57496441+TheoGuerin64@users.noreply.github.com>
2024-05-05 01:47:12 +0200

    Remove useless database initialization

--- server/app/__main__.py
+++ server/app/__main__.py
@@ -1,7 +1,5 @@
 import uvicorn
 
 from . import schemas  # noqa: F401
-from .db import Base, engine
 
-Base.metadata.create_all(bind=engine)
 uvicorn.run("app:app", ws_max_size=2**12, host="0.0.0.0", port=3000, reload=__debug__)
