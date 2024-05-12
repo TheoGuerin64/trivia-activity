d50453928ab74146740e7ad2152978e0efff16a8
TheoGuerin64 <57496441+TheoGuerin64@users.noreply.github.com>
2024-05-12 10:18:26 +0200

    Refactor connection store to use composition API

--- client/src/stores/connection.ts
+++ client/src/stores/connection.ts
@@ -2,30 +2,29 @@ import { defineStore } from 'pinia'
 
 import { sdk } from '@/sdk'
 import { socket } from '@/socket'
+import { ref } from 'vue'
 
-export const useConnectionStore = defineStore('connection', {
-  state: () => ({
-    connected: false,
-  }),
+export const useConnectionStore = defineStore('connection', () => {
+  const connected = ref(false)
 
-  actions: {
-    bindEvents() {
-      socket.on('connect', () => {
-        this.connected = true
-      })
+  function bindEvents() {
+    socket.on('connect', () => {
+      connected.value = true
+    })
 
-      socket.on('disconnect', () => {
-        this.connected = false
-      })
-    },
+    socket.on('disconnect', () => {
+      connected.value = false
+    })
+  }
 
-    connect(code: string) {
-      socket.auth = { code: code, channel_id: sdk.channelId }
-      socket.connect()
-    },
+  function connect(code: string) {
+    socket.auth = { code: code, channel_id: sdk.channelId }
+    socket.connect()
+  }
 
-    disconnect() {
-      socket.disconnect()
-    },
-  },
+  function disconnect() {
+    socket.disconnect()
+  }
+
+  return { connected, bindEvents, connect, disconnect }
 })
